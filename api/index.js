7938afdf552c39c5dcb07037e25397ec09f9ee4c
verizonold937-oss <verizonold937@gmail.com>
2026-06-05 19:20:49 +0700

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,9 @@
+export default function handler(req, res) {
+
+res.setHeader("Content-Type", "text/plain");
+
+res.status(200).send(
+`loadstring(game:HttpGet("https://raw.githubusercontent.com/verizonold937-oss/RexzHubGameSupport/main/RexzHubByRexz"))()`
+);
+
+}

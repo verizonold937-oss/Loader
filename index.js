457e73d568778f3b2fef822d058bb145e26af831
verizonold937-oss <verizonold937@gmail.com>
2026-06-05 19:19:49 +0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-
-res.setHeader("Content-Type", "text/plain");
-
-res.status(200).send(
-`loadstring(game:HttpGet("https://raw.githubusercontent.com/verizonold937-oss/RexzHubGameSupport/main/RexzHubByRexz"))()`
-);
-
-}

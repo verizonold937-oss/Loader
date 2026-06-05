edf394b30d3181815df378e8d15c6523f9ff2bb4
verizonold937-oss <verizonold937@gmail.com>
2026-06-05 19:23:00 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 export default function handler(req, res) {
 
-res.setHeader("Content-Type", "text/plain");
+  res.setHeader("Content-Type", "text/plain");
 
-res.status(200).send(
+  res.status(200).send(
 `loadstring(game:HttpGet("https://raw.githubusercontent.com/verizonold937-oss/RexzHubGameSupport/main/RexzHubByRexz"))()`
-);
+  );
 
 }
